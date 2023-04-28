86c0d56e95a8fadc1647adcd1002f87fd7f1c07a
gontarev <gontarevsanya@gmail.com>
2023-04-28 14:20:45 +0300

    hw 12 is done

--- front_end_pro_hw12/index.js
+++ front_end_pro_hw12/index.js
@@ -1,9 +1,12 @@
-const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-
-const key = generateKey(16, characters);
-console.log(key); // eg599gb60q926j8i
-
+const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
 
 function generateKey(length, characters) {
-    
+  let str = "";
+  for (let i=0; i < length; i++) {
+    str += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return str;
 }
+
+const key = generateKey(10, characters);
+console.log(key); 
