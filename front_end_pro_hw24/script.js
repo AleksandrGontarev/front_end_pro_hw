daeb93506caf3a1616aa8fae64dfb0357bbf6a75
gontarev <gontarevsanya@gmail.com>
2023-05-22 11:13:27 +0300

    fie

--- front_end_pro_hw24/script.js
+++ front_end_pro_hw24/script.js
@@ -1,14 +1,24 @@
 import ChangeInputValue from "./change_input.js";
 import createButton from "./create_btn.js";
 
+const http = 'http';
+const https = 'https';
 const input = document.querySelector('.input');
 
-createButton('https', "yellow");
-createButton('http', 'red');
+createButton(https, "yellow");
+createButton(http, 'red');
 
-document.querySelector('.button-https').addEventListener('click', function change(){new ChangeInputValue('https', 'http').value(input)}
+document.querySelector('.button-https').addEventListener('click', function change() {
+    new ChangeInputValue(https, http).value(input);
+    document.querySelector('.button-https').setAttribute('href', input.value);
+}
+
 );
 
-document.querySelector('.button-http').addEventListener('click', function change(){new ChangeInputValue('http', 'https').value(input)}
+document.querySelector('.button-http').addEventListener('click', function change() {
+    new ChangeInputValue(http, https).value(input);
+    document.querySelector('.button-http').setAttribute('href', input.value);
+}
 );
+
 
