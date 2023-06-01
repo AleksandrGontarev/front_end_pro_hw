5aeb35ed5e5585375ae09c72e1215fcd38cacb27
gontarev <gontarevsanya@gmail.com>
2023-06-01 18:25:15 +0300

    add settime

--- front_end_pro_hw26/buy_product.js
+++ front_end_pro_hw26/buy_product.js
@@ -3,7 +3,7 @@ function buyProduct(selectedProduct) {
     selectedProduct.addEventListener('click', (event) => {  
             let productName = event.target.classList[1].split('-')[1];
             alert(`Product ${productName} add to basket`);
-            window.location.reload();
+            window.setTimeout(() => window.location.reload(), 1000)
     }
     )
 }
